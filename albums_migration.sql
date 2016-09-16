81d22d5530f0952293bc3273baecc077407e901f
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-16 15:45:45 -0500

    Table is giving us the right input.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,15 @@
-Created file.
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist_name TEXT,
+	name_name TEXT,
+	releasedate_name DATE,
+	sales_name FLOAT NOT NULL,
+	genre_name TEXT NOT NULL,
+
+	PRIMARY KEY (id)
+
+);
