c3a6a69f1d7efb9cdf96ae2b9b74ddf651e0a9f9
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-18 14:53:27 -0500

    Seeder works, deleted period at the end of the last number in the seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -11,12 +11,12 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	artist VARCHAR(100) NOT NULL,
-	name VARCHAR(100) NOT NULL,
-	release_date INT UNSIGNED NOT NULL,
-	genre_name VARCHAR(100) NOT NULL,
-	sales_name FLOAT NOT NULL,
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`artist` VARCHAR(100) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`release_date` INT UNSIGNED NOT NULL,
+	`genre_name` VARCHAR(100) NOT NULL,
+	`sales_name` FLOAT NOT NULL,
 
 	PRIMARY KEY (id)
 
