4f30e8bf9d07e67b5e6305f9124170e1cad99d1c
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-18 14:14:56 -0500

    Table was created succesfully (the album migration)

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,14 +1,22 @@
 USE codeup_test_db;
 
+
+-- id — auto incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released (Note: MySQL also supports a YEAR data type!)
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	artist_name TEXT,
-	name_name TEXT,
-	releasedate_name DATE,
+	artist VARCHAR(100) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	release_date INT UNSIGNED NOT NULL,
+	genre_name VARCHAR(100) NOT NULL,
 	sales_name FLOAT NOT NULL,
-	genre_name TEXT NOT NULL,
 
 	PRIMARY KEY (id)
 
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
 
-INSERT INTO albums (artist, album, released, genre, sales data)
-VALUES ('Michael Jackson','Thriiller','1982','Pop','46'),
-		('AC/DC','Back in Black', '1980','Hard rock','26.1'),
-		('Pink Floyd','The Dark Side of the Moon','1973', 'Progressive rock','24.2'),
-		('Whitney Houston','The Bodyguard','1992','Soundtrack/R&B','27.4'),
-		('Meat Loaf', 'Bat Out of Hell','1977','Hard Rock','20.6.');
+INSERT INTO albums (artist_name, name_name, releasedate_name, genre_name, sales_name)
+VALUES ('Michael Jackson','Thriller',1982,'Pop',46),
+		('AC/DC','Back in Black', 1980,'Hard rock',26.1),
+		('Pink Floyd','The Dark Side of the Moon',1973, 'Progressive rock',24.2),
+		('Whitney Houston','The Bodyguard',1992,'Soundtrack/R&B',27.4),
+		('Meat Loaf', 'Bat Out of Hell',1977,'Hard Rock',20.6.);
