9639a70b939af83061963c24db4a44444b2aad2a
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-20 12:09:29 -0500

    Practiced using limits and offset

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,5 +1,7 @@
 /* In your script, use DISTINCT to find the unique titles in the titles table. */
 SELECT DISTINCT title FROM titles;
+/* or */
+SELECT title FROM titles GROUP BY title;
 /* Update the previous query to sort the results alphabetically. */
 SELECT DISTINCT title FROM titles GROUP BY title ASC;
 /* Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY */
