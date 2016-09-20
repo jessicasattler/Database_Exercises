bd729abc4f349186a27bfae0dfebeb5567e7a171
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-19 21:18:30 -0500

    The instructions and all the commands are shown in this file. This replaces the previous where_exercises file

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,11 @@
-
+/* employees with first names Irena, Vidya, or Maya */
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+/* employees whose last name starts with 'E' */
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+/* employees hired in the 90s */
 SELECT * FROM employees WHERE hire_date LIKE '199%';
-
-SELECT * FROM employees WHERE birth_date LIKE '%12-25';
-
+/* employees born on Christmas */
+SELECT * FROM employees WHERE birth_date LIKE '1%-12-25';
+/*  employees with a q in their last name  */
 SELECT * FROM employees WHERE last_name LIKE '%q%';
-
-
-
-
-
+ 
