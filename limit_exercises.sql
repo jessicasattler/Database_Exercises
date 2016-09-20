b16070fc60e6d07e008d597d16e758b1501832f6
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-20 12:23:43 -0500

    Edited the comments, batches work from 1 to 5 , 6 to 10, 11 to 15, etc and not as I had said in the previous exercise.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,5 +2,5 @@
 SELECT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
 /* Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. */
 SELECT first_name, last_name FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '1%-12-25' ORDER BY birth_date, hire_date DESC LIMIT 5;
-/* Try to think of your results as batches, or sets. The first five results are your first batch. The five after that would be your second batch, etc. Update the query to find the tenth batch of results */
+/* Try to think of your results as batches, or sets. The first five results are your first batch. The five after that would be your second batch, etc. Update the query to find the tenth batch of results The first batch would be 1 to 5, the second would be 6 to 10, etc.*/
 SELECT first_name, last_name FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '1%-12-25' ORDER BY birth_date, hire_date DESC LIMIT 5 OFFSET 45;
