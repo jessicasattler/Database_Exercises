9a1bf68fe965d687e5b6327275ea7a5658b795c0
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-20 09:51:53 -0500

    Chaining WHERE clauses to practice using AND and OR

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,13 @@
-/* employees with first names Irena, Vidya, or Maya */
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
-/* employees whose last name starts with 'E' */
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-/* employees hired in the 90s */
-SELECT * FROM employees WHERE hire_date LIKE '199%';
-/* employees born on Christmas */
-SELECT * FROM employees WHERE birth_date LIKE '1%-12-25';
-/*  employees with a q in their last name  */
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+/*Update your query for 'Irena' , 'Vidya' , or 'Maya' to use OR instead of IN */
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya'OR first_name ='Maya';
+/*Add a condition to find everybody with those names who is also male*/
+SELECT * FROM employees WHERE gender = 'm' AND (first_name = 'Irena' OR first_name = 'Vidya'OR first_name ='Maya');
+/* employees whose last name starts or ends with 'E' */
+SELECT * FROM employees WHERE last_name LIKE 'E%'OR last_name LIKE '%E';
+/* employees whose last name starts and ends with 'E' */
+SELECT * FROM employees WHERE last_name LIKE 'E%'AND last_name LIKE '%E';
+/* employees hired in the 90s AND born on Christmas */
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '1%-12-25';
+/* employees with a 'q' in their last name but not 'qu' */
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
  
